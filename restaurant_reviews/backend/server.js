7d1ee5671508b05d21ac4c47ca5472ae6378c3c7
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2021-08-22 14:47:10 -0700

    First commit

---
+++ restaurant_reviews/backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import restaurants from './api/restaurants.route.js'
+
+// Establishes the express application / server
+const app = express();
+
+app.use(cors());
+// Server can accept JSON in the body of a request
+app.use(express.json());
+
+app.use('/api/v1/restaurants', restaurants);
+// Asterisk means if a user tries to access a route that doesn't exist
+app.use('*', (req, res) => res.status(404).json({error: "Page not found."}));
+
+
+
+
+export default app;
